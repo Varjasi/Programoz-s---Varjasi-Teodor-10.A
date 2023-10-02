0eeee6e8eaf51b0b33b740cffb93c1204afeab72
Varjasi <szemanmarton4@gmail.com>
2023-10-02 11:44:54 +0200

    update 1.

---
+++ 4. óra.py
@@ -0,0 +1,13 @@
+#Keresés
+# Írd le a nevedet egy string-be és a program írja ki, hogy hányadik betű volt benne az a!
+
+nev = 'Teodor Varjasi'
+N = len(x)-1
+i = 0
+while i<=N and not (T(x[i])):
+    i += 1
+van = i<=N
+if van:
+    ind = i
+    ért = x[i]
+
