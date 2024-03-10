9ea0df92506cf51ace14810c60bc81bd2f6d56b2
Varjasi <144667167+Varjasi@users.noreply.github.com>
2024-03-10 22:37:46 +0100

    update 3

---
+++ 2024.03.04/1.py
@@ -0,0 +1,84 @@
+'''
+Feladatok:
+1. A beolvas fv-ben valósítsd meg, hogy a paraméterben megadott fájlt beolvassa, és térjen vissza a szám listával az átalakítások után. 
+2. A feladat1,2 stb. fv-ben valósítsd meg a kért eljárásokat! A fájl beolvasásához használd a beolvas() függvényedet! 
+3. feladat1(): számold ki a fájlokban található számok összegét a tanult prog tétel segítségével! 
+4. feladat2(): számold ki a fájlokban található számok átlagát a tanult prog tétel segítségével! 
+5. feladat3(): számold ki a fájlokban található számok minimumát a tanult prog tétel segítségével! 
+6. feladat4(): számold ki a fájlokban található számok maximumát a tanult prog tétel segítségével! 
+
+Input fájlok használata:
+feladat1() -> input1 
+feladat2() -> input2 
+feladat3() -> input3 
+feladat4() -> input3 
+
+Kiírások:
+feladat1: [eredmény] 
+feladat2: [eredmény] 
+feladat3: [eredmény] 
+feladat4: [eredmény] 
+A ": " után csak az eremény szerepeljen
+
+A dolgozat python fájlját küldd el a andrasi.norbert@puskas.hu-ra az alábbi tárggyal:
+[osztály][filebeolvasas1] név 
+'''
+
+def beolvas(fájlnév):
+    fájl = open(fájlnév, encoding="utf-8")
+    sorok = fájl.readlines()
+    fájl.close()
+    return sorok
+
+def feladat1(név):
+    lista = beolvas(név)
+    összeg = 0
+    for i in range(len(lista)):
+        összeg += int(lista[i])
+    return összeg
+def feladat2(szöveg, első, utolsó):
+    lista = beolvas(szöveg)
+    számok = []
+    for i in range(első,len(lista)-utolsó):
+        számok.append(int(lista[i].strip()))
+    átlag = 0
+    for i in range(len(számok)):
+        átlag += számok[i]
+    return átlag // len(lista)
+def feladat3(név, első, utolsó):
+    lista = beolvas(név)
+    számok = []
+    for i in range(első,len(lista)-utolsó):
+        számok.append(int(lista[i].strip()))
+    index = 0
+    minimum = számok[0]
+    for i in range(len(számok)):
+        if minimum > számok[i]:
+            index = i
+            minimum = számok[i]
+    return minimum, index
+def feladat4(név, első, utolsó):
+    lista = beolvas(név)
+    számok = []
+    for i in range(első,len(lista)-utolsó):
+        számok.append(int(lista[i].strip()))
+    index = 0
+    maximum = számok[0]
+    for i in range(len(számok)):
+        if maximum > számok[i]:
+            index = i
+            maximum = számok[i]
+    return index, maximum
+
+feladat01 = feladat1("2024.03.04/input1.txt")
+print("feladat1:", [(feladat01)])
+
+feladat02 = feladat2("2024.03.04/input2", 2, 0)
+print("feladat2:", [(feladat02)])
+
+feladat03 = feladat3("2024.03.04/input3", 1, 1)
+print("feladat3:", [(feladat03)])
+
+feladat03 = feladat3("2024.03.04/input3", 1, 1)
+print("feladat3:", [(feladat03)])
+
