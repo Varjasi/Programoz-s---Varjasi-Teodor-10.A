68537fdb79b4cd237b0303198d66c198c9be3cb3
Varjasi <teodorvarjasi.puskas@gmail.com>
2025-04-18 20:34:22 +0200

    update 1

--- 2024.05.20/1.py
+++ 2024.05.20/1.py
@@ -38,3 +38,27 @@ while not talalat:
         index2 += 1
 
 print(f'A listában lévő legnagyobb elem: {maximum}, helye: {pozicio}. pozíció')
+
+#3. feladat
+#Fájlkezelés
+
+adatok = []
+def adatokbeolvasása():
+    file = open('fuvarok.txt', 'r', encoding='utf-8')
+    for sor in file:
+        for oszlop in sor:
+            adat = oszlop.strip().split(' ')
+            adatok.append(adat)
+
+adatokbeolvasása()
+#print(adatok)
+def nullakm():
+    összeg = 0
+    for sor in adatok:
+        print(sor)
+        if sor == ['0']:
+            összeg = összeg + 1
+    return(összeg)
+     
+eredmeny = nullakm()
+print(f'Az adatbázisban {eredmeny} db 0 km havi futásteljesítményt tartalmazó hónap van')
